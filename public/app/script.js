908d4c0f226829f7373ca0c2231232666a287170
Kiruna13 <thomas.ducoulombier@free.fr>
2021-10-07 11:37:52 +0200

    Final design App

--- public/app/script.js
+++ public/app/script.js
@@ -8,19 +8,20 @@ let index = 0;
 
 containTask.innerHTML = "<div class='centered' id='tache'></div>";
 const currentTask = document.getElementById("tache");
-
+currentTask.innerHTML = "<p>"+taskArray[0]+"</p>";
 menuItems.forEach(item => {
 
   item.addEventListener('click', function(){
 
     if(taskArray.length > 0){
-      currentTask.innerHTML = "<p>"+taskArray[0]+"</p>";
+
       currentTask.style.animationName = "away";
       taskArray.shift();
       //containTask.innerHTML = "<div class='centered' id='tache'></div>";
       setTimeout(function(){
         currentTask.style.animationName = "";
         numTask +=1
+        currentTask.innerHTML = "<p>"+taskArray[0]+"</p>";
         if(taskArray.length == 0){
           currentTask.innerHTML = "<div class='noTasks'>Vous n'avez plus aucunes taches pour aujourd'hui.</div>";
         }
